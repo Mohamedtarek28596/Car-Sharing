f6e4a156d45566d64c5b1cb571e4c7bf65a5ab0c
Mohamedtarek28596 <42477938+Mohamedtarek28596@users.noreply.github.com>
2019-03-04 05:59:27 +0200

    .

--- app/src/main/java/com/example6767gh/mytestauthentication/ContactUs.java
+++ app/src/main/java/com/example6767gh/mytestauthentication/ContactUs.java
@@ -2,35 +2,52 @@ package com.example6767gh.mytestauthentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.Manifest;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.net.Uri;
 import android.content.pm.PackageManager;
 
 public class ContactUs extends AppCompatActivity {
     private Button bn;
+    private  Button bn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-        bn = (Button) findViewById(R.id.contactbutton);
-
+        bn = (Button) findViewById(R.id.callbutton);
+        bn2 = (Button) findViewById(R.id.fbbutton);
         bn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:011283557633"));
 
-                if (ActivityCompat.checkSelfPermission(ContactUs.this,
-                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:01283557633"));
                 startActivity(callIntent);
             }
         });
+        bn2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/100001114376487"));
+                    startActivity(intent);
+
+
+                } catch(Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/m7mdtarek96")));
+                    return;
+                }
+            }
+        });
+
+
+
+
 
     }
 }
